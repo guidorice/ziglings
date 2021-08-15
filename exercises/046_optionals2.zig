a1d093a3bdb4cbcd75ad66c3c367d5aec4f241cd
Alex G Rice <alex@radiant.earth>
2021-08-15 16:07:30 -0600

    ex. 45-52

--- exercises/046_optionals2.zig
+++ exercises/046_optionals2.zig
@@ -21,7 +21,7 @@ const std = @import("std");
 
 const Elephant = struct {
     letter: u8,
-    tail: *Elephant = null, // Hmm... tail needs something...
+    tail: ?*Elephant = null,
     visited: bool = false,
 };
 
@@ -47,12 +47,7 @@ fn visitElephants(first_elephant: *Elephant) void {
     while (!e.visited) {
         std.debug.print("Elephant {u}. ", .{e.letter});
         e.visited = true;
-
-        // We should stop once we encounter a tail that
-        // does NOT point to another element. What can
-        // we put here to make that happen?
-        if (e.tail == null) ???;
-
+        if (e.tail == null) break;
         e = e.tail.?;
     }
 }
